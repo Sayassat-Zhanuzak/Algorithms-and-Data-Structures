1e8eff8ac94b4a838d6e99da1c46409b380cf980
Zhanuzak Sayassat <sayassat.zhanuzak@gmail.com>
2025-09-24 23:01:26 +0500

    Solved Problem L

--- Lab_2/F.cpp
+++ Lab_2/F.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 class Node {
+    public:
     int data;
     Node *next, *prev;
 
@@ -14,15 +15,109 @@ class Node {
 };
 
 class Linked_list {
-    Linked_list *head, *tail;
+    public:
+    Node *tail, *head;
+    int size;
 
     Linked_list() {
         tail = NULL;
         head = NULL;
+        size = 0;
     }
 
-    void insert(int data, int pos) {
+    void push_back(int data) {
         Node *node = new Node(data);
-        
-    } 
+        if(tail == NULL) {
+            tail = head = node;
+        } else {
+            tail->next = node;
+            node->prev = tail;
+            tail = node;
+        }
+        size++;
+    }
+
+    void pop_front() {
+        if(head != NULL) {
+            head = head->next;
+            if(head != NULL) {
+                head->prev = NULL;
+            } else {
+                tail = NULL;
+            }
+            size--;
+        }
+    }
+
+    int front() {
+        return head->data;
+    }
+
+    bool empty() {
+        return (size == 0);
+    }
+
+    void insert(int pos_insert, int data) {
+        Node *curr = head;
+        int pos = 0;
+
+        while( curr != nullptr && pos < pos_insert) {
+            curr = curr->next;
+            pos++;
+        }
+
+        Node *newNode = new Node(data);
+
+        if(curr == nullptr) {
+            if(tail == nullptr) {
+                head = tail = newNode;
+            } else {
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
+        } else {
+            newNode->next = curr;
+            newNode->prev = curr->prev;
+
+            if(curr->prev != nullptr)
+                curr->prev->next = newNode;
+            else 
+                head = newNode;
+            curr->prev = newNode;
+        }
+        size++;
+    }
+};
+
+int main() {
+    int number;
+    int pos = 0;
+    int pos_insert;
+    cin >> number;
+    Linked_list *ll = new Linked_list();
+
+    for(int i = 0; i < number; i++) {
+        int data; 
+        cin >> data;
+        ll->push_back(data);
+    }
+
+    int data;
+    cin >> data >> pos_insert;
+
+    Node *curr = ll->head;
+    while(curr != nullptr) {
+        if(pos == pos_insert) {
+            ll->insert(pos_insert, data);
+            break;
+        }
+        curr = curr->next;
+        pos++;
+    }
+
+    while(!ll->empty()) {
+        cout << ll->front() << " ";
+        ll->pop_front();
+    }
 }
--- Lab_2/H.cpp
+++ Lab_2/H.cpp
@@ -1,123 +1,164 @@
 #include <iostream>
-
+ 
 using namespace std;
-
-class Node {
-    public:
-    int data;
-    Node *next, *prev;
-
-    Node(int data) {
-        this->data = data;
-        this->next = NULL;
-        this->prev = NULL;
-    }
+ 
+struct Node{
+    int val;
+    Node* next;
+    Node(): val(0), next(nullptr) {}
+    Node(int x): val(x), next(nullptr) {}
+    Node(Node* next): val(0), next(next) {}
+    Node(int x, Node* next): val(x), next(next) {}
 };
-
-class Linked_list {
-    public:
-    Node *tail, *head;
-    int size;
-
-    Linked_list() {
-        tail = NULL;
-        head = NULL;
-        size = 0;
+ 
+Node* insert(Node* head, Node* node, int p){
+    if(p == 0) {
+        node->next = head;
+        return node;
     }
-
-    void push_back(int data) {
-        Node *node = new Node(data);
-        if(tail == NULL) {
-            tail = head = node;
-        } else {
-            tail->next = node;
-            node->prev = tail;
-            tail = node;
-        }
-        size++;
-    }
-
-    void pop_front() {
-        if(head != NULL) {
-            head = head->next;
-            if(head != NULL) {
-                head->prev = NULL;
-            } else {
-                tail = NULL;
-            }
-            size--;
-        }
-    }
-
-    int front() {
-        return head->data;
-    }
-
-    bool empty() {
-        return (size == 0);
-    }
-
-    void insert(int pos_insert, int data) {
-        Node *curr = head;
-        int pos = 0;
-
-        while( curr != nullptr && pos < pos_insert) {
-            curr = curr->next;
-            pos++;
-        }
-
-        Node *newNode = new Node(data);
-
-        if(curr == nullptr) {
-            if(tail == nullptr) {
-                head = tail = newNode;
-            } else {
-                tail->next = newNode;
-                newNode->prev = tail;
-                tail = newNode;
-            }
-        } else {
-            newNode->next = curr;
-            newNode->prev = curr->prev;
-
-            if(curr->prev != nullptr)
-                curr->prev->next = newNode;
-            else 
-                head = newNode;
-            curr->prev = newNode;
-        }
-        size++;
-    }
-};
-
-int main() {
-    int number;
-    int pos = 0;
-    int pos_insert;
-    cin >> number;
-    Linked_list *ll = new Linked_list();
-
-    for(int i = 0; i < number; i++) {
-        int data; 
-        cin >> data;
-        ll->push_back(data);
-    }
-
-    int data;
-    cin >> data >> pos_insert;
-
-    Node *curr = ll->head;
-    while(curr != nullptr) {
-        if(pos == pos_insert) {
-            ll->insert(pos_insert, data);
-            break;
-        }
+    Node *curr = head;
+    for(int i = 0; i < p - 1; i++) {
         curr = curr->next;
-        pos++;
+    }
+    curr->next = curr->next;
+    curr->next = node;
+    return head;
+}
+ 
+Node* remove(Node* head, int p){
+    if(head == nullptr) return nullptr;
+    if(p == 0){
+        Node* tmp = head->next;
+        delete head;
+        return tmp;
+    }
+    Node* cur = head;
+    for(int i = 0; i < p-1; i++){
+        cur = cur->next;
+    }
+    Node* toDelete = cur->next;
+    cur->next = toDelete->next;
+    delete toDelete;
+    return head;
+
+}
+ 
+Node* replace(Node* head, int p1, int p2){
+    if(p1 == p2) return head;
+
+    Node* cur = head;
+    Node* toMove;
+    if(p1 == 0){
+        toMove = head;
+        head = head->next;
+    } else {
+        for(int i = 0; i < p1-1; i++) cur = cur->next;
+        toMove = cur->next;
+        cur->next = toMove->next;
     }
 
-    while(!ll->empty()) {
-        cout << ll->front() << " ";
-        ll->pop_front();
+    if(p2 == 0){
+        toMove->next = head;
+        head = toMove;
+    } else {
+        cur = head;
+        for(int i = 0; i < p2-1; i++) cur = cur->next;
+        toMove->next = cur->next;
+        cur->next = toMove;
     }
+    return head;
+
+}
+ 
+Node* reverse(Node* head){
+    Node* prev = nullptr;
+    Node* cur = head;
+    while(cur != nullptr){
+        Node* nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
+    return prev;
+
+}
+ 
+void print(Node* head){
+    if(head == nullptr){
+        cout << -1 << endl;
+        return;
+    }
+    Node* cur = head;
+    while(cur != nullptr){
+        cout << cur->val;
+        if(cur->next != nullptr) cout << " ";
+        cur = cur->next;
+    }
+    cout << endl;
+
+}
+ 
+Node* cyclic_left(Node* head, int x){
+    if(head == nullptr) return nullptr;
+
+    int len = 0;
+    Node* cur = head;
+    while(cur){ len++; cur = cur->next; }
+    x %= len;
+    if(x == 0) return head;
+
+    cur = head;
+    for(int i = 0; i < x-1; i++) cur = cur->next;
+    Node* newHead = cur->next;
+    cur->next = nullptr;
+
+    Node* tail = newHead;
+    while(tail->next) tail = tail->next;
+    tail->next = head;
+
+    return newHead;
+
+}
+ 
+Node* cyclic_right(Node* head, int x){
+    if(head == nullptr) return nullptr;
+
+    int len = 0;
+    Node* cur = head;
+    while(cur){ len++; cur = cur->next; }
+    x %= len;
+    if(x == 0) return head;
+
+    return cyclic_left(head, len - x);
+}
+ 
+int main(){
+    Node* head = nullptr;
+    while (true)
+    {
+        int command; cin >> command;
+        if (command == 0){
+            break;
+        }else if(command == 1){
+            int x, p; cin >> x >> p;
+            head = insert(head, new Node(x), p);
+        }else if (command == 2){
+            int p; cin >> p;
+            head = remove(head, p);
+        }else if (command == 3){
+            print(head);
+        }else if (command == 4){
+            int p1, p2; cin >> p1 >> p2;
+            head = replace(head, p1, p2);
+        }else if (command == 5){
+            head = reverse(head);
+        }else if (command == 6){
+            int x; cin >> x;
+            head = cyclic_left(head, x);
+        }else if (command == 7){
+            int x; cin >> x;
+            head = cyclic_right(head, x);
+        }   
+    }
+    return 0;
 }
