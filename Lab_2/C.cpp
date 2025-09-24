37e2481e4f682be60b831794150f14c641a443bc
Zhanuzak Sayassat <sayassat.zhanuzak@gmail.com>
2025-09-24 18:54:07 +0500

    Solved Problem C

--- Lab_2/C.cpp
+++ Lab_2/C.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 class Node {
     public:
-    int data; 
+    int data;
     Node *next, *prev;
 
-    Node(int data) {
+    Node(int data){
         this->data = data;
         this->next = NULL;
         this->prev = NULL;
@@ -16,36 +16,21 @@ class Node {
 class Linked_list {
     public:
     Node *tail, *head;
-    int sz;
-
     Linked_list() {
         tail = NULL;
         head = NULL;
     }
 
-    void push_front(int data) {
+    void push_back(int data) {
         Node *node = new Node(data);
-        if(tail == NULL) {
-            tail = node;
-            head = node;
-        } else {
+        if(tail != NULL) {
             tail->next = node;
             node->prev = tail;
             tail = node;
+        } else {
+            tail = node;
+            head = node;
         }
-        sz++;
-    }
-
-    void pop_back() {
-        if(tail != NULL) {
-            tail = tail->prev;
-            if(tail != NULL) {
-                tail->next = NULL;
-            } else {
-                tail = NULL;
-            }
-        }
-        sz--;
     }
 
     void pop_front() {
@@ -53,6 +38,17 @@ class Linked_list {
             head = head->next;
             if(head != NULL) {
                 head->prev = NULL;
+            } else {
+                tail = NULL;
+            }
+        }
+    }
+
+    void pop_back() {
+        if(tail != NULL) {
+            tail = tail->prev;
+            if(tail != NULL) {
+                tail->next = NULL;
             } else {
                 head = NULL;
             }
@@ -62,55 +58,51 @@ class Linked_list {
     void del_node(Node *node) {
         if(node == tail) {
             pop_back();
+        }
+        else if(node == head) {
+            pop_front();
         } else {
             Node *a = node->prev;
             Node *b = node->next;
             a->next = b;
             b->prev = a;
         }
-        sz--;
     }
 
-    Node* addr_head() {
-        return head;
-    }
-
-    bool empty() {
-        return (sz == 0);
-    }
-
-    int front() {
-        return head->data;
-    }
+    void print() {
+        Node *node = head;
+        while(node != NULL) {
+            cout << node->data << " ";
+            node = node->next;
+        }
+        cout << endl;
+    }   
 };
 
+
 int main() {
+    Linked_list *ll = new Linked_list();
     int num;
     cin >> num;
-
-    Linked_list *ll = new Linked_list();
 
     for(int i = 0; i < num; i++) {
         int a;
         cin >> a;
-        ll->push_front(a);
+        ll->push_back(a);
     }
 
-    int i = 1;
-    Node *current = ll->addr_head();
-    while(current != nullptr) {
-        Node *nextNode = current->next;
+    Node *curr = ll->head;
 
-        if(i % 2 == 0) {
-            ll->del_node(current);
+    int pos = 1;
+    while(curr != NULL) {
+        Node *nextnode = curr->next;
+        if(pos % 2 == 0) {
+            ll->del_node(curr);
         }
-        current = nextNode;
-        i++;
-    } 
-
-    while(!ll->empty()) {
-        cout << ll->front() << " ";
-        ll->pop_front();
-
+        curr = nextnode;
+        pos++;
     }
+
+    ll->print();
+    return 0;
 }
